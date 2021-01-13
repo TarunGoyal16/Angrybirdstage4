80f562c09886d5546a5fa9ed115a0b376ea51a03
TarunGoyal16 <75246965+TarunGoyal16@users.noreply.github.com>
2021-01-13 14:52:37 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,25 @@
+class SlingShot{
+    constructor(body1,point2)
+    {
+        var options={
+            bodyA:body1,
+            pointB:point2,
+            stifness:0.1,
+            length:10
+        }
+        this.pointB=point2
+        this.sling=Constraint.create(options)
+        World.add(world,this.sling)
+        
+    }
+    fly(){
+        this.sling.bodyA=null
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA=this.sling.bodyA.position
+        var pointB=this.pointB
+        strokeWeight(2)
+        line(pointA.x,pointA.y,pointB.x,pointB.y)}
+    }
+}
